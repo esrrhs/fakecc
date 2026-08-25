f12fa08978cda7c880be88dabc5887075b4ad75c
esrrhs <esrrhs@163.com>
2026-08-26 05:27:22 +0800

    feat: port pr63302 from GCC torture execute suite
    - Port gcc.c-torture/execute/pr63302.c to test/e2e/cases/torture/execute/gcc_torture_pr63302.c
    - Remove pr63302 from UNSUPPORTED.txt

--- test/e2e/cases/torture/execute/gcc_torture_pr63302.c
+++ test/e2e/cases/torture/execute/gcc_torture_pr63302.c
@@ -1,19 +1,40 @@
-/* PR tree-optimization/63302 */
-
 // expect: 0
 package main;
 
-__attribute__((noinline)) int
+/* PR tree-optimization/63302 */
+
+extern void abort (void);
+
+int
+foo (__int128 x)
+{
+  __int128 v = x & (((__int128) -1 << 63) | 0x7ff);
+
+  return v == 0 || v == ((__int128) -1 << 63);
+}
+
+int
 bar (long long x)
 {
   long long v = x & (((long long) -1 << 31) | 0x7ff);
- 
+
   return v == 0 || v == ((long long) -1 << 31);
 }
 
 int
 main (void)
 {
+  if (foo (0) != 1
+      || foo (1) != 0
+      || foo (0x800) != 1
+      || foo (0x801) != 0
+      || foo ((__int128) 1 << 63) != 0
+      || foo ((__int128) -1 << 63) != 1
+      || foo (((__int128) -1 << 63) | 1) != 0
+      || foo (((__int128) -1 << 63) | 0x800) != 1
+      || foo (((__int128) -1 << 63) | 0x801) != 0)
+    abort ();
+
   if (bar (0) != 1
       || bar (1) != 0
       || bar (0x800) != 1
@@ -23,6 +44,7 @@ main (void)
       || bar ((-1LL << 31) | 1) != 0
       || bar ((-1LL << 31) | 0x800) != 1
       || bar ((-1LL << 31) | 0x801) != 0)
-    __builtin_abort ();
+    abort ();
+
   return 0;
 }
