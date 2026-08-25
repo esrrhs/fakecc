1302dbbb5f2c88f8c723b1c08bad36e17a8dae8b
esrrhs <esrrhs@163.com>
2026-08-26 01:26:59 +0800

    feat: port pr97386-1 from GCC torture execute suite
    - Port gcc.c-torture/execute/pr97386-1.c to test/e2e/cases/torture/execute/gcc_torture_pr97386_1.c
    - Remove pr97386-1 from UNSUPPORTED.txt

--- test/e2e/cases/torture/execute/gcc_torture_pr97386_1.c
+++ test/e2e/cases/torture/execute/gcc_torture_pr97386_1.c
@@ -1,15 +1,21 @@
-/* PR rtl-optimization/97386 */
-
 // expect: 0
 package main;
 
-static unsigned char foo(unsigned int c) {
-  return __builtin_bswap16((unsigned long long)(0xccccLLU << c | 0xccccLLU >> ((-c) & 63)));
+/* PR rtl-optimization/97386 */
+
+extern void abort (void);
+
+unsigned char
+foo (unsigned int c)
+{
+  return __builtin_bswap16 ((unsigned long long) (0xccccLLU << c | 0xccccLLU >> ((-c) & 63)));
 }
 
-int main(void) {
-  unsigned char x = foo(0);
+int
+main ()
+{
+  unsigned char x = foo (0);
   if (x != 0xcc)
-    __builtin_abort();
+    abort ();
   return 0;
 }
