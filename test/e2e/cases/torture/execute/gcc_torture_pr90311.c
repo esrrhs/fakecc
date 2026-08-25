00979f556f93504664551b05193c5c0d6b86cec8
esrrhs <esrrhs@163.com>
2026-08-26 03:57:29 +0800

    feat: port pr90311 from GCC torture execute suite
    - Port gcc.c-torture/execute/pr90311.c to test/e2e/cases/torture/execute/gcc_torture_pr90311.c
    - Remove pr90311 from UNSUPPORTED.txt

--- test/e2e/cases/torture/execute/gcc_torture_pr90311.c
+++ test/e2e/cases/torture/execute/gcc_torture_pr90311.c
@@ -1,17 +1,21 @@
-/* PR rtl-optimization/90311 */
-
 // expect: 0
 package main;
 
+/* PR rtl-optimization/90311 */
+
+extern void abort (void);
+
 int a, b;
 
-int main(void) {
+int
+main (void)
+{
   unsigned long long x;
   unsigned int c;
-  __builtin_add_overflow((unsigned char)a, b, &c);
-  b -= c < (unsigned char)a;
+  __builtin_add_overflow ((unsigned char) a, b, &c);
+  b -= c < (unsigned char) a;
   x = b;
   if (x)
-    __builtin_abort();
+    abort ();
   return 0;
 }
