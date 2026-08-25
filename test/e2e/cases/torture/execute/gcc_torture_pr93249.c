99ccfb7327f524722036dea6d547839ef3968aee
esrrhs <esrrhs@163.com>
2026-08-26 04:20:58 +0800

    feat: port pr93249 from GCC torture execute suite
    - Port gcc.c-torture/execute/pr93249.c to test/e2e/cases/torture/execute/gcc_torture_pr93249.c
    - Remove pr93249 from UNSUPPORTED.txt

--- test/e2e/cases/torture/execute/gcc_torture_pr93249.c
+++ test/e2e/cases/torture/execute/gcc_torture_pr93249.c
@@ -1,35 +1,46 @@
-/* PR tree-optimization/93249 */
-
 // expect: 0
 package main;
 
-static char a[2], b[4], c[6];
+/* PR tree-optimization/93249 */
 
-static void foo(void) {
+extern void abort (void);
+extern char *strncpy (char *, const char *, unsigned long);
+
+char a[2], b[4], c[6];
+
+void
+foo (void)
+{
   char d[2] = { 0x00, 0x11 };
-  __builtin_strncpy(&b[2], d, 2);
-  __builtin_strncpy(&b[1], a, 2);
+  strncpy (&b[2], d, 2);
+  strncpy (&b[1], a, 2);
   if (b[0] || b[1] || b[2] || b[3])
-    __builtin_abort();
+    abort ();
 }
 
-static void bar(void) {
-  __builtin_strncpy(&b[2], "\0\x11", 2);
-  __builtin_strncpy(&b[1], a, 2);
+void
+bar (void)
+{
+  strncpy (&b[2], "\0\x11", 2);
+  strncpy (&b[1], a, 2);
   if (b[0] || b[1] || b[2] || b[3])
-    __builtin_abort();
+    abort ();
 }
 
-static void baz(void) {
-  __builtin_strncpy(&c[2], "\x11\x11\0\x11", 4);
-  __builtin_strncpy(&c[1], a, 2);
+void
+baz (void)
+{
+  strncpy (&c[2], "\x11\x11\0\x11", 4);
+  strncpy (&c[1], a, 2);
   if (c[0] || c[1] || c[2] || c[3] != 0x11 || c[4] || c[5])
-    __builtin_abort();
+    abort ();
 }
 
-int main(void) {
-  foo();
-  bar();
-  baz();
+int
+main (void)
+{
+  foo ();
+  bar ();
+  baz ();
   return 0;
 }
