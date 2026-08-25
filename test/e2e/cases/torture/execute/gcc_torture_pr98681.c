7f45037075d1905bf763c98e0177def9b7aa7fc3
esrrhs <esrrhs@163.com>
2026-08-26 03:15:51 +0800

    feat: port pr98681 from GCC torture execute suite
    - Port gcc.c-torture/execute/pr98681.c to test/e2e/cases/torture/execute/gcc_torture_pr98681.c
    - Remove pr98681 from UNSUPPORTED.txt

--- test/e2e/cases/torture/execute/gcc_torture_pr98681.c
+++ test/e2e/cases/torture/execute/gcc_torture_pr98681.c
@@ -1,17 +1,23 @@
-/* PR target/98681 */
-
 // expect: 0
 package main;
 
-static int foo(int x) {
+/* PR target/98681 */
+
+extern void abort (void);
+
+int
+foo (int x)
+{
   if (x > 32)
     return (x << -64) & 255;
   else
     return x;
 }
 
-int main(void) {
-  if (foo(32) != 32 || foo(-150) != -150)
-    __builtin_abort();
+int
+main ()
+{
+  if (foo (32) != 32 || foo (-150) != -150)
+    abort ();
   return 0;
 }
