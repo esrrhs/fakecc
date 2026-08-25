86267264eea993f1608c091b554261859b43b717
esrrhs <esrrhs@163.com>
2026-08-26 04:04:42 +0800

    feat: port pr91635 from GCC torture execute suite
    - Port gcc.c-torture/execute/pr91635.c to test/e2e/cases/torture/execute/gcc_torture_pr91635.c
    - Remove pr91635 from UNSUPPORTED.txt

--- test/e2e/cases/torture/execute/gcc_torture_pr91635.c
+++ test/e2e/cases/torture/execute/gcc_torture_pr91635.c
@@ -1,44 +1,56 @@
-/* PR target/91635 */
-
 // expect: 0
 package main;
+
+/* PR target/91635 */
+
+extern void abort (void);
 
 unsigned short b, c;
 int u, v, w, x;
 
-static int foo(unsigned short c) {
-  c <<= __builtin_add_overflow(-c, -1, &b);
+int
+foo (unsigned short c)
+{
+  c <<= __builtin_add_overflow (-c, -1, &b);
   c >>= 1;
   return c;
 }
 
-static int bar(unsigned short b) {
+int
+bar (unsigned short b)
+{
   b <<= -14 & 15;
   b = b >> -~1;
   return b;
 }
 
-static int baz(unsigned short e) {
+int
+baz (unsigned short e)
+{
   e <<= 1;
-  e >>= __builtin_add_overflow(8719476735, u, &v);
+  e >>= __builtin_add_overflow (8719476735, u, &v);
   return e;
 }
 
-static int qux(unsigned int e) {
+int
+qux (unsigned int e)
+{
   c = ~1;
   c *= e;
   c = c >> (-15 & 5);
   return c + w + x;
 }
 
-int main(void) {
-  if (foo(0xffff) != 0x7fff)
-    __builtin_abort();
-  if (bar(5) != 5)
-    __builtin_abort();
-  if (baz(~0) != 0x7fff)
-    __builtin_abort();
-  if (qux(2) != 0x7ffe)
-    __builtin_abort();
+int
+main (void)
+{
+  if (foo (0xffff) != 0x7fff)
+    abort ();
+  if (bar (5) != 5)
+    abort ();
+  if (baz (~0) != 0x7fff)
+    abort ();
+  if (qux (2) != 0x7ffe)
+    abort ();
   return 0;
 }
