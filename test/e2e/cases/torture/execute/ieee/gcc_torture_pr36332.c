824d754edafe9e6c7c58f4466904673333fc4b98
esrrhs <esrrhs@163.com>
2026-08-26 15:27:40 +0800

    feat(torture): port ieee/pr36332

---
+++ test/e2e/cases/torture/execute/ieee/gcc_torture_pr36332.c
@@ -0,0 +1,14 @@
+// expect: 0
+package main;
+int
+foo (long double ld)
+{
+  return ld == __builtin_infl ();
+}
+int
+main ()
+{
+  if (foo (1.18973149535723176502126385303097021e+4932L))
+    __builtin_abort ();
+  return 0;
+}
