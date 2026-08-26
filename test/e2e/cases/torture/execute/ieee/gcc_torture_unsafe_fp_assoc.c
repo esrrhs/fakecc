d2238703a11ac228c9900af56e14fd53d944a6fa
esrrhs <esrrhs@163.com>
2026-08-26 19:39:14 +0800

    test: port gcc torture test execute/ieee/unsafe-fp-assoc

--- test/e2e/cases/torture/execute/ieee/gcc_torture_unsafe_fp_assoc.c
+++ test/e2e/cases/torture/execute/ieee/gcc_torture_unsafe_fp_assoc.c
@@ -1,15 +1,20 @@
 // expect: 0
 package main;
+
 extern void abort(void);
-static const double C = ((double)1.79769313486231570814527423731704357e+308L);
+
+static const double C = 1.7976931348623157e+308;
+
 double foo(double x)
 {
         return ( ( (x * C) * C ) * C);
 }
+
 int main ()
 {
   double d = foo (0.0);
   if (d != 0.0)
    abort ();
+
   return 0;
 }
