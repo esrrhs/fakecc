57d6d19e5274e253be1d8641cf64cd2370c56a62
esrrhs <esrrhs@163.com>
2026-08-26 19:38:42 +0800

    test: port gcc torture test execute/ieee/pr84235

--- test/e2e/cases/torture/execute/ieee/gcc_torture_pr84235.c
+++ test/e2e/cases/torture/execute/ieee/gcc_torture_pr84235.c
@@ -1,11 +1,16 @@
 // expect: 0
 package main;
+
+/* PR tree-optimization/84235 */
+
+extern void abort(void);
+
 int
 main ()
 {
   double d = 1.0 / 0.0;
   _Bool b = d == d && (d - d) != (d - d);
   if (!b)
-    __builtin_abort ();
+    abort ();
   return 0;
 }
