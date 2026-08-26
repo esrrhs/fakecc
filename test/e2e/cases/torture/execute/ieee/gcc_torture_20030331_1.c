b310fea131a59bde655b00253e252ace3607394f
esrrhs <esrrhs@163.com>
2026-08-26 19:24:27 +0800

    test: port gcc torture test execute/ieee/20030331-1

--- test/e2e/cases/torture/execute/ieee/gcc_torture_20030331_1.c
+++ test/e2e/cases/torture/execute/ieee/gcc_torture_20030331_1.c
@@ -1,30 +1,35 @@
 // expect: 0
 package main;
+
 extern void exit (int);
 extern void abort (void);
 float x = -1.5f;
+
 float
 rintf ()
 {
-  static const float TWO23 = 8388608.0;
-  if (__builtin_fabs (x) < TWO23)
+  static const float TWO23 = 8388608.0f;
+
+  if (__builtin_fabsf (x) < TWO23)
     {
-      if (x > 0.0)
+      if (x > 0.0f)
         {
           x += TWO23;
           x -= TWO23;
         }
-      else if (x < 0.0)
+      else if (x < 0.0f)
         {
           x = TWO23 - x;
           x = -(x - TWO23);
         }
     }
+
   return x;
 }
+
 int main (void)
 {
-  if (rintf () != -2.0)
+  if (rintf () != -2.0f)
     abort ();
   exit (0);
 }
