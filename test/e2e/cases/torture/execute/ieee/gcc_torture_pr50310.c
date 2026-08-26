2d8765b294f37d5556b69bcdf3ef826e5b4c902d
esrrhs <esrrhs@163.com>
2026-08-26 19:36:34 +0800

    test: port gcc torture test execute/ieee/pr50310

--- test/e2e/cases/torture/execute/ieee/gcc_torture_pr50310.c
+++ test/e2e/cases/torture/execute/ieee/gcc_torture_pr50310.c
@@ -1,7 +1,11 @@
 // expect: 0
 package main;
+
+/* PR target/50310 */
+
 extern void abort (void);
 double s1[4], s2[4], s3[64];
+
 void
 foo (void)
 {
@@ -39,6 +43,7 @@ foo (void)
   for (i = 0; i < 4; i++)
     s3[15 * 4 + i] = s1[i] >= s2[i] ? -1.0 : 0.0;
 }
+
 int
 main ()
 {
@@ -51,21 +56,19 @@ main ()
   s2[1] = 5.0;
   s2[2] = 5.0;
   s2[3] = 5.0;
-  asm volatile ("" : : : "memory");
   foo ();
-  asm volatile ("" : : : "memory");
   for (i = 0; i < 16 * 4; i++)
     if (i >= 12 * 4 && (i & 3) == 3)
       {
- if (s3[i] != 0.0) abort ();
+	if (s3[i] != 0.0) abort ();
       }
     else
       {
         static int masks[] = { 2, 2|4, 1, 1|4, 1|2, 8, 2, 1 };
         if (s3[i]
-     != (((1 << (i & 3)) & ((i & 4) ? ~masks[i / 8] : masks[i / 8]))
-  ? -1.0 : 0.0))
-   abort ();
+	    != (((1 << (i & 3)) & ((i & 4) ? ~masks[i / 8] : masks[i / 8]))
+		? -1.0 : 0.0))
+	  abort ();
       }
   return 0;
 }
