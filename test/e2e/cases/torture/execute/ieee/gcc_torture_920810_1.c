f53ce2f084e20ed63a22c6fb180f88d6beab30fb
esrrhs <esrrhs@163.com>
2026-08-26 19:19:49 +0800

    test: port gcc torture test execute/ieee/920810-1

--- test/e2e/cases/torture/execute/ieee/gcc_torture_920810_1.c
+++ test/e2e/cases/torture/execute/ieee/gcc_torture_920810_1.c
@@ -2,7 +2,8 @@
 package main;
 
 extern int sprintf(char *str, const char *format, ...);
+extern int strcmp(const char *s1, const char *s2);
 void abort (void);
 void exit (int);
 double normalize(x)double x;{if(x==0)x=0;return x;}
-int main(void){char b[9];sprintf(b,"%g",normalize(-0.0));if(__builtin_strcmp(b,"0"))abort();exit(0);}
+int main(void){char b[9];sprintf(b,"%g",normalize(-0.0));if(strcmp(b,"0"))abort();exit(0);}
