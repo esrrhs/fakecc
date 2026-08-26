a8a0fcadcf8a5b77920638f42a0667356e21f091
esrrhs <esrrhs@163.com>
2026-08-26 19:23:29 +0800

    test: port gcc torture test execute/ieee/20010226-1

--- test/e2e/cases/torture/execute/ieee/gcc_torture_20010226_1.c
+++ test/e2e/cases/torture/execute/ieee/gcc_torture_20010226_1.c
@@ -1,17 +1,19 @@
 // expect: 0
 package main;
+
 void abort (void);
+
 long double dfrom = 1.1L;
 long double m1;
 long double m2;
 unsigned long mant_long;
+
 int main()
 {
-  if (1.08420217248550443400745280086994171e-19L > 0x1p-31L)
-    return 0;
   m1 = dfrom / 2.0L;
   m2 = m1 * 4294967296.0L;
   mant_long = ((unsigned long) m2) & 0xffffffff;
+
   if (mant_long == 0x8ccccccc)
     return 0;
   else
