ff6f30cedc70740f91cc8fb19af4bbf3bd5d084f
esrrhs <esrrhs@163.com>
2026-08-26 15:03:39 +0800

    feat(torture): port ieee/980619-1

---
+++ test/e2e/cases/torture/execute/ieee/gcc_torture_980619_1.c
@@ -0,0 +1,23 @@
+// expect: 0
+package main;
+ void abort (void);
+ void exit (int);
+ int main(void)
+ {
+  float reale = 1.0f;
+  float oneplus;
+  int i;
+   if (sizeof (float) != 4)
+    exit (0);
+  for (i = 0; ; i++)
+  {
+   oneplus = 1.0f + reale;
+   if (oneplus == 1.0f)
+    break;
+   reale=reale/2.0f;
+  }
+  if (i != 24)
+    abort ();
+  else
+    exit (0);
+ }
