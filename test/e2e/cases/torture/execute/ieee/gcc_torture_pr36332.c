779b7e76d51597d4ea9e2ca0206ead952703b035
esrrhs <esrrhs@163.com>
2026-08-26 19:34:13 +0800

    test: port gcc torture test execute/ieee/pr36332

--- test/e2e/cases/torture/execute/ieee/gcc_torture_pr36332.c
+++ test/e2e/cases/torture/execute/ieee/gcc_torture_pr36332.c
@@ -1,14 +1,20 @@
 // expect: 0
 package main;
+
+/* PR target/36332 */
+
+extern void abort(void);
+
 int
 foo (long double ld)
 {
   return ld == __builtin_infl ();
 }
+
 int
 main ()
 {
-  if (foo (1.18973149535723176502126385303097021e+4932L))
-    __builtin_abort ();
+  if (foo (1.18973149535723176502e+4932L))
+    abort ();
   return 0;
 }
