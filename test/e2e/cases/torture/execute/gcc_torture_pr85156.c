350a8b7a32bbec4efcb2f04cde96fd24df80d86d
esrrhs <esrrhs@163.com>
2026-08-26 09:51:19 +0800

    feat: port pr85156 from GCC torture execute suite
    - Port gcc.c-torture/execute/pr85156.c to test/e2e/cases/torture/execute/gcc_torture_pr85156.c
    - Remove pr85156 from UNSUPPORTED.txt

--- test/e2e/cases/torture/execute/gcc_torture_pr85156.c
+++ test/e2e/cases/torture/execute/gcc_torture_pr85156.c
@@ -1,19 +1,25 @@
-/* PR tree-optimization/85156 */
-
 // expect: 0
 package main;
 
+/* PR tree-optimization/85156 */
+
+extern void abort (void);
+
 int x, y;
 
-static int foo(int z) {
-  if (__builtin_expect(x ? y != 0 : 0, z++))
+int
+foo (int z)
+{
+  if (__builtin_expect (x ? y != 0 : 0, z++))
     return 7;
   return z;
 }
 
-int main(void) {
+int
+main (void)
+{
   x = 1;
-  if (foo(10) != 11)
-    __builtin_abort();
+  if (foo (10) != 11)
+    abort ();
   return 0;
 }
