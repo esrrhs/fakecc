bd6508e4b524f01f8722ba9e8200cbc891555ef3
esrrhs <esrrhs@163.com>
2026-08-26 08:23:56 +0800

    feat: port pr65427 from GCC torture execute suite
    - Port gcc.c-torture/execute/pr65427.c to test/e2e/cases/torture/execute/gcc_torture_pr65427.c
    - Remove pr65427 from UNSUPPORTED.txt

---
+++ test/e2e/cases/torture/execute/gcc_torture_pr65427.c
@@ -0,0 +1,40 @@
+// expect: 0
+package main;
+
+/* PR tree-optimization/65427 */
+
+extern void abort (void);
+extern int memcmp (const void *, const void *, unsigned long);
+
+typedef int V __attribute__ ((vector_size (8 * sizeof (int))));
+V a, b, c, d, e, f;
+
+void
+foo (int x, int y)
+{
+  do
+    {
+      if (x)
+	d = a ^ c;
+      else
+	d = a ^ b;
+    }
+  while (y);
+}
+
+int
+main (void)
+{
+  a = (V) { 1, 2, 3, 4, 5, 6, 7, 8 };
+  b = (V) { 0x40, 0x80, 0x40, 0x80, 0x40, 0x80, 0x40, 0x80 };
+  e = (V) { 0x41, 0x82, 0x43, 0x84, 0x45, 0x86, 0x47, 0x88 };
+  foo (0, 0);
+  if (memcmp (&d, &e, sizeof (V)) != 0)
+    abort ();
+  c = (V) { 0x80, 0x40, 0x80, 0x40, 0x80, 0x40, 0x80, 0x40 };
+  f = (V) { 0x81, 0x42, 0x83, 0x44, 0x85, 0x46, 0x87, 0x48 };
+  foo (1, 0);
+  if (memcmp (&d, &f, sizeof (V)) != 0)
+    abort ();
+  return 0;
+}
