a29d0c04ab560f33582a6041d046a1f2ba56db7a
esrrhs <esrrhs@163.com>
2026-08-26 10:08:28 +0800

    feat: port pr86528 from GCC torture execute suite
    - Port gcc.c-torture/execute/pr86528.c to test/e2e/cases/torture/execute/gcc_torture_pr86528.c
    - Remove pr86528 from UNSUPPORTED.txt

--- test/e2e/cases/torture/execute/gcc_torture_pr86528.c
+++ test/e2e/cases/torture/execute/gcc_torture_pr86528.c
@@ -1,19 +1,29 @@
-/* PR middle-end/86528 */
-
 // expect: 0
 package main;
 
-static void test(char *data, unsigned long len) {
-  static const char appended[] = "/./";
-  char *buf = __builtin_alloca(len + sizeof(appended));
-  __builtin_memcpy(buf, data, len);
-  __builtin_strcpy(buf + len, &appended[data[len - 1] == '/']);
-  if (__builtin_strcmp(buf, "test1234/./"))
-    __builtin_abort();
+/* PR middle-end/86528 */
+
+extern void abort (void);
+extern void *memcpy (void *, const void *, unsigned long);
+extern char *strcpy (char *, const char *);
+extern int strcmp (const char *, const char *);
+extern unsigned long strlen (const char *);
+
+void
+test (char *data, unsigned long len)
+{
+  static char const appended[] = "/./";
+  char *buf = __builtin_alloca (len + sizeof appended);
+  memcpy (buf, data, len);
+  strcpy (buf + len, &appended[data[len - 1] == '/']);
+  if (strcmp (buf, "test1234/./"))
+    abort ();
 }
 
-int main(void) {
+int
+main (void)
+{
   char *arg = "test1234/";
-  test(arg, __builtin_strlen(arg));
+  test (arg, strlen (arg));
   return 0;
 }
