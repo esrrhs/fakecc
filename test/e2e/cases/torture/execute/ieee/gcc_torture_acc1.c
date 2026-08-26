797443ada0df94497e73c10f86a80c20ca344b61
esrrhs <esrrhs@163.com>
2026-08-26 19:18:18 +0800

    test: port gcc torture test execute/ieee/acc1

--- test/e2e/cases/torture/execute/ieee/gcc_torture_acc1.c
+++ test/e2e/cases/torture/execute/ieee/gcc_torture_acc1.c
@@ -1,7 +1,11 @@
 // expect: 0
 package main;
+
+/* Tail call optimizations would reverse the order of additions in func().  */
+
 void abort (void);
 void exit (int);
+
 double func (const double *array)
 {
   double d = *array;
@@ -10,6 +14,7 @@ double func (const double *array)
   else
     return d + func (array + 1);
 }
+
 int main ()
 {
   double values[] = { 0.1e-100, 1.0, -1.0, 0.0 };
