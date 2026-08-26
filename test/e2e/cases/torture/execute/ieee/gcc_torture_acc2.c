4e602af1b82e5fbf9b0379e036b37e498d49edcb
esrrhs <esrrhs@163.com>
2026-08-26 19:18:43 +0800

    test: port gcc torture test execute/ieee/acc2

--- test/e2e/cases/torture/execute/ieee/gcc_torture_acc2.c
+++ test/e2e/cases/torture/execute/ieee/gcc_torture_acc2.c
@@ -1,7 +1,12 @@
 // expect: 0
 package main;
+
+/* Tail call optimizations would reverse the order of multiplications
+   in func().  */
+
 void abort (void);
 void exit (int);
+
 double func (const double *array)
 {
   double d = *array;
@@ -10,10 +15,11 @@ double func (const double *array)
   else
     return d * func (array + 1);
 }
+
 int main ()
 {
-  double values[] = { ((double)1.79769313486231570814527423731704357e+308L), 2.0, 0.5, 1.0 };
-  if (func (values) != ((double)1.79769313486231570814527423731704357e+308L))
+  double values[] = { 1.7976931348623157e+308, 2.0, 0.5, 1.0 };
+  if (func (values) != 1.7976931348623157e+308)
     abort ();
   exit (0);
 }
