927bac2639375112893a7b267412082a50591fcf
esrrhs <esrrhs@163.com>
2026-08-26 19:38:06 +0800

    test: port gcc torture test execute/ieee/pr72824-2

--- test/e2e/cases/torture/execute/ieee/gcc_torture_pr72824_2.c
+++ test/e2e/cases/torture/execute/ieee/gcc_torture_pr72824_2.c
@@ -1,6 +1,12 @@
 // expect: 0
 package main;
+
+/* PR tree-optimization/72824 */
+
 typedef float V __attribute__((vector_size (4 * sizeof (float))));
+
+extern void abort(void);
+
 static inline void
 foo (V *x, V value)
 {
@@ -8,12 +14,13 @@ foo (V *x, V value)
   for (i = 0; i < 32; ++i)
     x[i] = value;
 }
+
 int
 main ()
 {
   V x[32];
   foo (x, (V) { 0.f, -0.f, 0.f, -0.f });
-  if (__builtin_copysignf (1.0, x[3][1]) != -1.0f)
-    __builtin_abort ();
+  if (__builtin_copysignf (1.0f, x[3][1]) != -1.0f)
+    abort ();
   return 0;
 }
