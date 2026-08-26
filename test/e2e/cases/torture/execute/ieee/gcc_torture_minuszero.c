0ed794fa1d430faafaa9ec1d7dee72472eb14e87
esrrhs <esrrhs@163.com>
2026-08-26 15:18:50 +0800

    feat(torture): port ieee/minuszero

---
+++ test/e2e/cases/torture/execute/ieee/gcc_torture_minuszero.c
@@ -0,0 +1,23 @@
+// expect: 0
+package main;
+void abort (void);
+void exit (int);
+int
+main (void)
+{
+  union
+    {
+      double d;
+      unsigned short i[sizeof (double) / sizeof (short)];
+    } u;
+  int a = 0;
+  int b = -5;
+  int j;
+  u.d = (double) a / b;
+  for (j = 0; j < sizeof (double) / sizeof (short); j++)
+    {
+      if (u.i[j] == 0x8000)
+ exit (0);
+    }
+  abort ();
+}
