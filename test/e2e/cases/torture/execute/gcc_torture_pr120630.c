ab2c6755dbf07a2c818e5bc60e9ef0d32356ab19
esrrhs <esrrhs@163.com>
2026-08-26 14:40:05 +0800

    feat(torture): port pr120630

---
+++ test/e2e/cases/torture/execute/gcc_torture_pr120630.c
@@ -0,0 +1,27 @@
+// expect: 0
+package main;
+__attribute__((noipa)) int
+foo (const char *x, ...)
+{
+  return *x;
+}
+int a, b, c;
+unsigned d = 1;
+int
+main ()
+{
+  if (a)
+    foo ("0");
+  int e = -1;
+  if (a < 1)
+    {
+      e = c;
+      if (c)
+ while (1)
+   ;
+    }
+  b = (~e + 0UL) / -1;
+  if (d > b)
+    __builtin_abort ();
+  return 0;
+}
