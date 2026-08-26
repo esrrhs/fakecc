cd054b2b5ee4a1d20ca29e2ba297f2a7af0206cd
esrrhs <esrrhs@163.com>
2026-08-26 19:37:34 +0800

    test: port gcc torture test execute/ieee/pr72824

--- test/e2e/cases/torture/execute/ieee/gcc_torture_pr72824.c
+++ test/e2e/cases/torture/execute/ieee/gcc_torture_pr72824.c
@@ -1,5 +1,10 @@
 // expect: 0
 package main;
+
+/* PR tree-optimization/72824 */
+
+extern void abort(void);
+
 static inline void
 foo (float *x, float value)
 {
@@ -7,12 +12,13 @@ foo (float *x, float value)
   for (i = 0; i < 32; ++i)
     x[i] = value;
 }
+
 int
 main ()
 {
   float x[32];
   foo (x, -0.f);
-  if (__builtin_copysignf (1.0, x[3]) != -1.0f)
-    __builtin_abort ();
+  if (__builtin_copysignf (1.0f, x[3]) != -1.0f)
+    abort ();
   return 0;
 }
